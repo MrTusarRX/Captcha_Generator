8573385a936227090af0c85d27f9726ba344d9ff
tusar khan <114900895+MrTusarRX@users.noreply.github.com>
2025-07-03 18:29:55 +0530

    Update img_gen.php

--- captcha-generator/img_gen.php
+++ captcha-generator/img_gen.php
@@ -1,7 +1,7 @@
 <?php
 //STARTING  SESSION
   //GENERATING RANDOM 4 CHARACTER FOR CAPTCHA
-    $string='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhijklmnopqrstuvwxyz1234567890';
+    $string='ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
     $string_shuff=str_shuffle($string);
     $text=substr($string_shuff,0,4);
 
