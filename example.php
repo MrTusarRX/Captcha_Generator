87758fabf2904974dd1adf50760cb0f48a72f908
tusar khan <114900895+MrTusarRX@users.noreply.github.com>
2025-07-03 18:18:49 +0530

    Update example.php

--- example.php
+++ example.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Example Captcha</title>
-  </head>
-  <body>
-    <form method = "POST" >
-		<div id="ae_captcha_api"></div>
-		<div><input type="text" placeholder = "Enter Captcha" name="user_input"/></div>
-		<div><input type="submit" formaction="./validate.php" value = "Submit"/></div>
-	</form>
-	<script src="./captcha-generator/asset/main.js"></script>
-  </body>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Example Captcha</title>
+</head>
+<body>
+
+  <form method="POST" action="validate.php">
+    <div id="ae_captcha_api"></div>
+    <div>
+      <input type="text" name="user_input" placeholder="Enter Captcha" required />
+    </div>
+    <div>
+      <input type="submit" value="Submit" />
+    </div>
+  </form>
+
+  <script src="./captcha-generator/asset/main.js"></script>
+
+</body>
 </html>
